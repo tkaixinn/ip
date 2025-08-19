8393f72a5c00b1bf0644a4d8af898d3383cda4d2
tkaixinn <teokaixin128@gmail.com>
2025-08-19 10:40:56 +0800

    Finished Level-2

--- Chatterbox.java
+++ Chatterbox.java
@@ -8,11 +8,21 @@ public class Chatterbox {
 	Scanner scanner = new Scanner(System.in);
 	String message = scanner.nextLine();
 
-	while (!message.equals("bye")) { 
-	    System.out.println(" " + message);
-	    message = scanner.nextLine();
-	}
+	String[] list = new String[100];
+        int listCount = 0;
 
+	while (!message.equals("bye")) { 
+	    if (message.equals("list")) {
+		for (int i = 0; i < listCount; i++) {
+		    System.out.println((i + 1) + "." + list[i]);
+		}
+	    } else { 		
+	    System.out.println("added: " + message);
+	    list[listCount] = message;
+            listCount++;
+	    }
+	message = scanner.nextLine();
+	}
 	System.out.println("Bye. Hope to see you again soon!");
 	scanner.close();	
     }
