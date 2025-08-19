0bc0b029a118ab280ea9d5738270143effa142ea
tkaixinn <teokaixin128@gmail.com>
2025-08-19 10:15:15 +0800

    Finished Level-1

--- Chatterbox.java
+++ Chatterbox.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Chatterbox {
     public static void main(String[] args) {
 	System.out.println("Hello! I'm Chatterbox");
 	System.out.println("What can I do for you?");
 
+	Scanner scanner = new Scanner(System.in);
+	String message = scanner.nextLine();
+
+	while (!message.equals("bye")) { 
+	    System.out.println(" " + message);
+	    message = scanner.nextLine();
+	}
+
 	System.out.println("Bye. Hope to see you again soon!");
-	
+	scanner.close();	
     }
 }
 
