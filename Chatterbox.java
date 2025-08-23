908cfa993f508b3a2d87a7a02945185296365cc6
tkaixinn <teokaixin128@gmail.com>
2025-08-23 12:51:00 +0800

    Finished A-MoreOOP

--- Chatterbox.java
+++ Chatterbox.java
@@ -1,118 +1,82 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Chatterbox {
+
     public static void main(String[] args) {
-	System.out.println("Hello! I'm Chatterbox");
-	System.out.println("What can I do for you?");
+        Ui ui = new Ui();
+        Storage storage = new Storage();
+        Task[] tasks = storage.loadTasks();
+        TaskList taskList = new TaskList(tasks, Database.getListCount());
 
-	Scanner scanner = new Scanner(System.in);
+        ui.showWelcome();
 
-	Task[] list = new Task[100];
-        int listCount = 0;
-        listCount = Database.loadTasks(list);
+        while (true) {
+            String message = ui.readCommand();
+            String commandType = Parser.getCommandType(message);
 
-	while (scanner.hasNextLine()) {
-		String message = scanner.nextLine();
-	
-	try {
-	     if (message.equals("todo")) {
-	         throw new TodoException("SORRY!!! No empty description for a todo");
-	      }
+            try {
+                switch (commandType) {
+                    case "bye":
+                        ui.showMessage("Bye. Hope to see you again soon!");
+                        ui.close();
+                        return;
+                    case "list":
+                        taskList.printList(ui);
+                        break;
+                    case "todo":
+                        if (message.equals("todo")) throw new TodoException("No empty description for a todo");
+			Todo newTodo = new Todo(message.split(" ", 2)[1]);
+                        taskList.addTask(newTodo);
 
-	     if (message.equals("deadline")) {
-		 throw new DeadlineException("SORRY!!! No empty description for a deadline");
-	     }
+                        ui.showMessage("Got it. I've added this task:");
+			ui.showMessage(" " + newTodo);
+			ui.showMessage("Now you have " + taskList.getListCount() + " tasks in the list.");
+                        break;
+                    case "deadline":
+                        if (message.equals("deadline")) throw new DeadlineException("No empty description for a deadline");
+                        Deadline newDeadline = (new Deadline(message.split(" ", 2)[1]));
+			taskList.addTask(newDeadline);
 
-	     if (message.equals("event")) {
-		 throw new EventException("SORRY!!! No empty description for an event");
-	     }
-	
-	 } catch (TodoException | DeadlineException |EventException e) {
-	          System.out.println(e.getMessage());
-		  continue;
- 	 }
-	     
+                        ui.showMessage("Got it. I've added this task:");
+			ui.showMessage(" " + newDeadline);
+			ui.showMessage("Now you have " + taskList.getListCount() + " tasks in the list.");
+                        break;
+                    case "event":
+                        if (message.equals("event")) throw new EventException("No empty description for an event");
+                        String[] fromParts = message.split("/from", 2);
+                        String[] dateParts = fromParts[1].split("/to", 2);
+                        String fromTime = "From: " + dateParts[0].trim();
+                        String toTime = "To: " + dateParts[1].trim();
 
-	     if (message.equals("bye")) {
-	         System.out.println("Bye. Hope to see you again soon!");
-		 System.out.flush();
-		 break;
-		}
+			Event newEvent = new Event(fromParts[0].replaceFirst("event", "").trim(), fromTime, toTime);
+                        taskList.addTask(newEvent);
 
-             if (message.contains("delete")) {
-                ArrayList<Task> newList = new ArrayList<>(Arrays.asList(list));
-                String[] indexArr = message.split("\\s", 2);
-                int index = Integer.parseInt(indexArr[1]);
-                newList.remove(index - 1);
-                list = newList.toArray(new Task[0]);
-		listCount--;
-		Database.saveTasks(list, listCount);
-		continue;
-	        }
+			ui.showMessage("Got it. I've added this task:");
+			ui.showMessage(" " + newEvent);
+			ui.showMessage("Now you have " + taskList.getListCount() + " tasks in the list.");
+                        break;
+                    case "delete":
+                        int index = Integer.parseInt(message.split(" ")[1]) - 1;
+                        taskList.deleteTask(index);
+                        ui.showMessage("Task deleted!");
+                        break;
+                    case "mark":
+                        index = Integer.parseInt(message.split(" ")[1]) - 1;
+                        taskList.markTask(index);
+                        ui.showMessage("Task marked done!");
+                        break;
+                    case "unmark":
+                        index = Integer.parseInt(message.split(" ")[1]) - 1;
+                        taskList.unmarkTask(index);
+                        ui.showMessage("Task unmarked!");
+                        break;
+                    default:
+                        throw new NilException("I don't know what that means");
+                }
+            } catch (Exception e) {
+                ui.showMessage(e.getMessage());
+            }
 
-
-	    if (message.equals("list")) {
-		System.out.println("Here are the tasks in your list:");
-		for (int i = 0; i < listCount; i++) {
-		    System.out.println((i + 1) + "." + list[i]);
-		}
-		
-	    } else if (message.contains("mark")) {
-		String[] parts = message.split("\\s+");
-		    int trueTaskIndex = Integer.parseInt(parts[1]) - 1;
-                    Task trueTask = list[trueTaskIndex];
-    
-                if (parts[0].equals("mark")) {
-		    trueTask.isDone = true;
-		    System.out.println("Nice! I've marked this task as done:");
-		    System.out.println(trueTask);
-		    Database.saveTasks(list, listCount);
-		} else if (parts[0].equals("unmark")) {
-		    trueTask.isDone = false;
-		    System.out.println("OK, I've marked this task as not done yet:");
-		    System.out.println(trueTask);
-		    Database.saveTasks(list, listCount);
-		}
-	    } else {
-			Task currentTask = null;
-
-			if (message.contains("todo")) {
-				String[] parts = message.split("\\s", 2);
-				currentTask = new Todo(parts[1]);
-
-			} else if (message.contains("deadline")) {
-				String[] parts = message.split("\\s", 2);
-				currentTask = new Deadline(parts[1]);
-				
-
-			} else if (message.contains("event")) {
-				String[] fromParts = message.split("/from", 2);
-				String[] dateParts = fromParts[1].split("/to", 2);
-			        String fromTime = "From: " + dateParts[0].trim();
-                                String toTime = "To: " + dateParts[1].trim();
-				currentTask = new Event(fromParts[0].replaceFirst("event", "").trim(), fromTime, toTime);
-			} else {
-				try {
-			            throw new NilException("SORRY!!! I don't know what that means");
-				} catch (NilException e) {
-				    System.out.println(e.getMessage());
-				    continue;
-				}    
-			} 
-
-			if (currentTask != null) {
-				System.out.println("Got it. I've added this task:");
-				System.out.println(currentTask);
-				list[listCount] = currentTask;
-				listCount++;
-				Database.saveTasks(list, listCount);
-				System.out.println("Now you have " + listCount + " tasks in the list.");
-			}
-		}
-	}
-	scanner.close();	
+            storage.saveTasks(taskList.getList(), taskList.getListCount());
+        }
     }
 }
 
---
+++ Ui.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        System.out.println("Hello! I'm Chatterbox");
+        System.out.println("What can I do for you?");
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void showLoadingError() {
+        System.out.println("Error loading tasks from file!");
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
